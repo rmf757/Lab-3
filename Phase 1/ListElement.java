415168a43cc2f093b2255a96b7ab5fe3a710f20b
Ranil Fernando <ranilmf@gmail.com>
2017-10-14 16:56:55 -0600

    Linked List was created

---
+++ Phase 1/ListElement.java
@@ -0,0 +1,11 @@
+public class ListElement {
+    pricate ListElement next;
+    private ListElement previous;
+    private int data;
+
+    public ListElement() {
+        this.data = 0;
+        this.next = null;
+        this.previous = null;
+    }
+}
