104485710be6070d1af91e825c1a83ffd70e99d4
Ranil Fernando <ranilmf@gmail.com>
2017-10-14 20:38:37 -0600

    Major Changes, reorganized everything into 1 file and restarted on a lot of code

--- Phase 1/ListElement.java
+++ Phase 1/ListElement.java
@@ -1,25 +1,98 @@
-public class ListElement {
-    private ListElement next;
-    private int data;
+public class List {
+    
+	public static class ListElement {
+	
+		private ListElement next;
+		private int data;
+		private static ListElement first;
+		private static ListElement last;
 
-    public ListElement() {
-        this.data = 0;
-        this.next = null;
+		public ListElement() {
+			data = 0;
+			next = null;
+		}
+
+		public void setData(int d) {
+			data = d;
+		}
+
+		public int getData() {
+			return data;
+		}
+		
+		public static void insert(ListElement n) {
+            ListElement a = new ListElement();
+            a = n;
+			a.nextlink = null;
+            if (first != null) {
+                last.nextlink = a;
+                last = a;
+            } else {
+                first = a;
+                last = a;
+            }
+            System.out.println(d);
+        }
+		
+	
+	}
+	
+
+    private int size;
+	
+	
+
+    public void addElement(ListElement le) {
+        size++;
+
+        if(begin == null){
+            begin = le;
+            end = begin;
+        }
+        else {
+			System.out.print(begin.data);
+            end.setNext(le);
+            end = le;		
+        }
+		System.out.print(end.getData());
     }
 
-    public void setData(int data) {
-        this.data = data;
+    public void printLinkedListHead() {
+        ListElement temp = new ListElement();
+        //temp = begin; 
+		System.out.print("\n");
+        System.out.print(begin.data);
+		//begin = begin.getNext();
+		System.out.print(end.data);
+		/*
+        while (temp.getNext() != null) {
+           System.out.print(temp.getNext());
+           temp = temp.getNext();
+        }       */
     }
+	
+	public static void main(String[] args) {
+		//LinkedList test = new LinkedList();
+		
+        ListElement le = new ListElement();
+        le.setData(5);
+		le.setNext(null);
+		le.addElement(le);
+		//System.out.print(le.getData());
+        le.setData(4);
+		le.setNext(null);
+		le.addElement(le);
+		//System.out.print(le.getData());
+        le.setData(3);
+		le.setNext(null);
+		le.addElement(le);
+		//System.out.print(le.getData());
+        le.setData(2);
+        le.setNext(null);
+		le.addElement(le);
+		//System.out.print(le.getData());
+        le.printLinkedListHead();
 
-    public void setNext(ListElement n) {
-        this.next = n;
     }
-
-    public ListElement getNext() {
-        return this.next;
-    }
-
-    public int getData() {
-        return this.data;
-    }
+	
 }
