1d8fad150ba72dac06c025aa0aa49464f1ba065b
Ranil Fernando <ranilmf@gmail.com>
2017-10-14 20:59:38 -0600

    Reimplemented the UI class and fixed the linked list to properly add elements

--- Phase 1/ListElement.java
+++ Phase 1/ListElement.java
@@ -1,6 +1,4 @@
-public class List {
-    
-	public static class ListElement {
+	public class ListElement {
 	
 		private ListElement next;
 		private int data;
@@ -11,7 +9,14 @@ public class List {
 			data = 0;
 			next = null;
 		}
-
+		
+		public ListElement(int i, ListElement n) {
+		
+			data = i;
+			next = n;
+			
+		}
+		
 		public void setData(int d) {
 			data = d;
 		}
@@ -20,10 +25,10 @@ public class List {
 			return data;
 		}
 		
-		public static void insert(ListElement n) {
-            ListElement a = new ListElement();
-            a = n;
-			a.next = null;
+		public static void addElement(int n) {
+            ListElement a = new ListElement(n, null);
+            //a = n;
+			//a.next = null;
             if (first != null) {
                 last.next = a;
                 last = a;
@@ -33,65 +38,12 @@ public class List {
             }
         }
 		
-	
-	}
-	
-
-    private int size;
-	
-	
-
-    public void addElement(ListElement le) {
-        size++;
-
-        if(begin == null){
-            begin = le;
-            end = begin;
+		public static void printLinkedListHead() {
+            System.out.println("Linked List Elements\n");
+            ListElement temp = first;
+            while (temp != null) {
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
         }
-        else {
-			System.out.print(begin.data);
-            end.setNext(le);
-            end = le;		
-        }
-		System.out.print(end.getData());
-    }
-
-    public void printLinkedListHead() {
-        ListElement temp = new ListElement();
-        //temp = begin; 
-		System.out.print("\n");
-        System.out.print(begin.data);
-		//begin = begin.getNext();
-		System.out.print(end.data);
-		/*
-        while (temp.getNext() != null) {
-           System.out.print(temp.getNext());
-           temp = temp.getNext();
-        }       */
-    }
-	
-	public static void main(String[] args) {
-		//LinkedList test = new LinkedList();
-		
-        ListElement le = new ListElement();
-        le.setData(5);
-		le.setNext(null);
-		le.addElement(le);
-		//System.out.print(le.getData());
-        le.setData(4);
-		le.setNext(null);
-		le.addElement(le);
-		//System.out.print(le.getData());
-        le.setData(3);
-		le.setNext(null);
-		le.addElement(le);
-		//System.out.print(le.getData());
-        le.setData(2);
-        le.setNext(null);
-		le.addElement(le);
-		//System.out.print(le.getData());
-        le.printLinkedListHead();
-
-    }
-	
 }
---
+++ Phase 1/UI.java
@@ -0,0 +1,19 @@
+public class UI {
+
+	public static void main(String[] args) {
+		//LinkedList test = new LinkedList();
+		
+        ListElement le = new ListElement();
+   
+		le.addElement(5);
+	
+		le.addElement(4);
+
+		le.addElement(3);
+
+		le.addElement(2);
+
+        le.printLinkedListHead();
+
+	}
+}
